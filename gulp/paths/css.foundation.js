22c948a2ba2650d813f6e6e779f4f3d1e043bbfc
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-09-13 14:18:21 +0300

    version 2.1

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css'
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/animate.css/animate.min.css',
+  './node_modules/font-awesome/css/font-awesome.min.css'
 ];
